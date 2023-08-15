7d430f27be3a63aa2515e4bd7dc56ea1dd527502
Cria <victor55cria@gmail.com>
2023-08-14 21:59:05 -0300

    usando massad de dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,9 @@
 /// <reference types="cypress" />
 
+const { InternetModule, fa } = require('@faker-js/faker');
+const perfil = require('../fixtures/perfil.json')
+
+
 context('Login feito com sucesso', () =>{
 
     beforeEach(() => {
@@ -10,10 +14,33 @@ context('Login feito com sucesso', () =>{
 
     it('deve fazer login com sucesso', () => {
            cy.get('#username').type('aluno_ebac@teste.com')
-           cy.get('#password'). type('teste@teste.com')
+           cy.get('#password').type('teste@teste.com')
            cy.get('.woocommerce-form > .button').click()
            cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a').should('contain', 'Logout')
     })
+
+    
+    it.only('Deve fazer login com sucesso - usando fixture ', () => {
+        cy.fixture('perfil').then(dados => {
+            
+            cy.get('#username').type(dados.usuario,{log: false})
+            cy.get('#password').type(dados.senha,{log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a').should('contain', 'Logout')
+
+        })
+
+     });
+
+
+
+    it('Deve fazer login ok com dados importados ', () => {
+        
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a').should('contain', 'Logout')
+    });
 
     it('deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
         cy.get('#username').type('aluno_ebac222@teste.com')
